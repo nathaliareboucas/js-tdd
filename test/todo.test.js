8cf381cad9bda12d29b142f0fe6c4052cf2bcb64
Nathália Rebouças <nathreboucas5@gmail.com>
2021-02-22 11:05:54 -0300

    correção teste criação de todo

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,9 +1,18 @@
-const { describe, it, before } = require('mocha');
+const { describe, it, beforeEach, afterEach } = require('mocha');
 const { expect } = require('chai');
+const {createSandbox} = require('sinon');
 
 const Todo = require('../src/todo');
 
 describe('todo', () => {
+  beforeEach(() => {
+    sandbox = createSandbox();
+  })
+
+  afterEach(() => {
+    sandbox.restore();
+  })
+
   describe('#isValid', () => {
     it('should return invalid when creating an object without "text"', () => {
       const data = {
@@ -30,9 +39,25 @@ describe('todo', () => {
         text: 'Hello',
         when: new Date("2020-12-01")        
       }
+      const expectedId = '000001';
+      
+      const uuid = require('uuid');
+      const fakeUUID = sandbox.fake.returns(expectedId);
+      sandbox.replace(uuid, 'v4', fakeUUID);
+
       const todo = new Todo(data);
+      const expectedItem = {
+        text: data.text,
+        when: data.when,
+        status: '',
+        id: expectedId
+      }
+
       const result = todo.isValid();
       expect(result).to.be.ok;
+
+      expect(uuid.v4.calledOnce).to.be.ok;
+      expect(todo).to.be.deep.equal(expectedItem);
     })
   })
 });
